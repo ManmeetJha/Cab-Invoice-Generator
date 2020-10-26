4cbed762c91ecd72385ee24c414f509efe2a4c3e
Navneet <navneetjh5@gmail.com>
2020-10-27 01:59:22 +0530

    ADDED: Calculating fare function

--- src/main/java/CabInvoiceGenerator.java
+++ src/main/java/CabInvoiceGenerator.java
@@ -1,5 +1,14 @@
 public class CabInvoiceGenerator {
+    private static final double MINIMUM_COST_PER_KM = 10.0;
+    private static final int COST_PER_TIME = 1;
+    private static final double MINIMUM_FARE = 5.0;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Cab invoice generator");
+    }
+
+    public double calculateFare(double distance,int time) {
+        double totalFare=distance*MINIMUM_COST_PER_KM+time*COST_PER_TIME+MINIMUM_FARE;
+        return totalFare;
     }
 }
--- src/test/java/InvoiceTest.java
+++ src/test/java/InvoiceTest.java
@@ -1,2 +1,20 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class InvoiceTest {
+    CabInvoiceGenerator cabInvoiceGenerator;
+
+    @Before
+    public void setUp() throws Exception {
+        cabInvoiceGenerator=new CabInvoiceGenerator();
+    }
+
+    @Test
+    public void givenDistanceAndTime_ShouldReturnTotalFare() {
+        double distance=10.0;
+        int time=100;
+        double fare = cabInvoiceGenerator.calculateFare(distance,time);
+        Assert.assertEquals(205,fare,0);
+    }
 }
